aa01cf2cb97b495f1ef6f810068f1217b657f75a
Naex <rafaelpiedrahita414@gmail.com>
2024-05-31 11:10:24 -0500

    Finalización de refactorización en BlackJack de Vite

--- src/blackjack/usecases/index.js
+++ src/blackjack/usecases/index.js
@@ -1,3 +1,6 @@
-import { crearDeck } from './crear-deck';
-import { pedirCarta } from './pedir-carta';
-import { valorCarta } from './valor-carta';
+
+export { crearCartaHTML } from './crear-carta-html';
+export { crearDeck } from './crear-deck';
+export { pedirCarta } from './pedir-carta';
+export { turnoComputadora } from './turno-computadora';
+export { valorCarta } from './valor-carta';
