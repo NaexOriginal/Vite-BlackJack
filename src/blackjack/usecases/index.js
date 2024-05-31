2c178bbb9edc019e7d914b15a121d556751ef011
Naex <rafaelpiedrahita414@gmail.com>
2024-05-30 20:46:53 -0500

    Refactorizacion de ciertas partes del codigo

---
+++ src/blackjack/usecases/index.js
@@ -0,0 +1,3 @@
+import { crearDeck } from './crear-deck';
+import { pedirCarta } from './pedir-carta';
+import { valorCarta } from './valor-carta';
---
+++ src/blackjack/usecases/pedir-carta.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @param {Array<String>} deck es un arreglo de String
+ * @returns {String} retorna la carta del Deck
+ */
+
+
+export const pedirCarta = (deck) => {
+
+
+    if ( !deck || deck.length === 0 ) {
+        throw 'No hay cartas en el deck';
+    }
+    const carta = deck.pop();
+    return carta;
+}
